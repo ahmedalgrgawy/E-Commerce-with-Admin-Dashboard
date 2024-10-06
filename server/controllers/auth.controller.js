a01554ffa34d9632b8c926008ba93eb460135a4b
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-10-06 19:49:41 +0300

    Login

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -41,6 +41,44 @@ export const signup = async (req, res) => {
 
 }
 
+export const login = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" })
+        }
+
+        const isMatch = await user.comparePassword(password)
+
+        if (!isMatch) {
+            return res.status(401).json({ success: false, message: "Invalid credentials" })
+        }
+
+        // authentication - Store Token in Redis & Cookies
+        const { accessToken, refreshToken } = generateToken(user._id)
+
+        await storeToken(user._id, refreshToken)
+
+        storeTokenInCookies(res, accessToken, refreshToken)
+
+        res.status(200).json({
+            success: true, message: "Logged in successfully", user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        })
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
+    }
+
+}
+
 export const logout = async (req, res) => {
     try {
         if (req.cookies.refreshToken) {
