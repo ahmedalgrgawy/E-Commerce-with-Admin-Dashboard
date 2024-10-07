ff5a8dda320e1b6ac9ea0e0d546536deab5ae240
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-10-07 14:57:09 +0300

    Featured Products

--- server/controllers/products.controllers.js
+++ server/controllers/products.controllers.js
@@ -1,3 +1,4 @@
+import redis from "../lib/redis.js";
 import Product from "../models/products.model.js"
 export const getAllProducts = async (req, res) => {
     try {
@@ -8,5 +9,35 @@ export const getAllProducts = async (req, res) => {
 
     } catch (error) {
         return res.status(500).json({ message: error.message })
+    }
+}
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+
+        let featuredProducts = await redis.get("featuredProducts");
+
+        if (featuredProducts) {
+            return res.status(200).json(JSON.parse(featuredProducts))
+        }
+
+        featuredProducts = await Product.find({ featured: true }).sort({ createdAt: -1 }).lean();
+
+        if (!featuredProducts) return res.status(404).json({ message: "No featured products found" })
+
+        await redis.set("featuredProducts", JSON.stringify(featuredProducts));
+
+        return res.status(200).json(featuredProducts)
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
+
+export const createProduct = async (req, res) => {
+    try {
+
+    } catch (error) {
+
     }
 }
--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { getAllProducts } from '../controllers/products.controllers.js';
+import { getAllProducts, getFeaturedProducts,createProduct } from '../controllers/products.controllers.js';
 import { adminRoute, protectedRoute } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.get("/", protectedRoute, adminRoute, getAllProducts)
+
+router.get("/featured-products", getFeaturedProducts)
+
+router.post("/create-product", protectedRoute, adminRoute, createProduct)
 
 export default router;
