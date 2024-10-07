a70993f5908c3a51b2c1b096d5e99cd631df4ec5
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-10-07 16:37:39 +0300

    Fixing

--- server/routes/coupon.routes.js
+++ server/routes/coupon.routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { protectedRoute, validateCoupon } from '../middlewares/auth.middleware.js';
+import { protectedRoute } from '../middlewares/auth.middleware.js';
+import { getCoupon } from '../controllers/coupon.controller.js';
 
 const router = express.Router();
 
