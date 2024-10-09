ca7010bf85f25e86be7c1443f480e0455b2d0d7c
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-10-09 17:39:59 +0300

    Get By Category Logic

--- client/src/components/ProductsList.jsx
+++ client/src/components/ProductsList.jsx
@@ -6,9 +6,6 @@ export const ProductsList = () => {
 
     const { deleteProduct, toggleFeature, products } = useAdminStore();
 
-    console.log(products);
-
-
     return (
         <motion.div
             className='bg-gray-800 shadow-lg rounded-lg overflow-hidden max-w-4xl mx-auto'
--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -1,15 +1,19 @@
 import { useEffect } from "react"
 import { useProductsStore } from "../stores/products"
+import { useParams } from "react-router-dom"
 
 export const CategoryPage = () => {
 
     const { filteredProducts, getProductsByCategory } = useProductsStore()
 
+    const { category } = useParams()
+
     useEffect(() => {
-        getProductsByCategory('bags')
-    }, [getProductsByCategory])
+        getProductsByCategory(category)
+    }, [category, getProductsByCategory])
 
     console.log(filteredProducts);
+
 
     return (
         <div>
