4760edfa980fe9c56cae5f8068c3a34fe3868f46
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-10-10 19:02:23 +0300

    Finish Project

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 import cors from "cors";
+import path from 'path'
 import { connectDb } from './lib/connectDb.js';
 import authRouter from './routes/auth.routes.js'
 import productsRouter from './routes/products.routes.js'
@@ -14,6 +15,8 @@ dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const __dirname = path.resolve()
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 
@@ -32,6 +35,15 @@ app.use('/api/coupon', couponRoutes)
 app.use("/api/payments", paymentRoutes);
 
 app.use("/api/analytics", analyticRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+    });
+
+}
 
 app.listen(port, () => {
     connectDb()
